1ec94caff71d6703b1e387e66554934e1856ff1d
niyioladeji <niyioladeji@hotmail.com>
2022-07-09 02:12:07 +0100

    rnm-0002 # Added skeletal method and tests for integer to roman numeral converter

---
+++ RomanNumeralConverterApi/src/main/java/com/adeniyi/rnconverter/service/RomanNumeralConverterService.java
@@ -0,0 +1,21 @@
+package com.adeniyi.rnconverter.service;
+
+public class RomanNumeralConverterService {
+
+    public String getRomanNumeralForInteger(int inputNumber) {
+        return null;
+    }
+    String getRomanUnit(Character unitCharacter) {
+        return null;
+    }
+    String getRomanTenth(Character tens) {
+        return null;
+    }
+    String getRomanHundredth(Character hundredth) {
+        return null;
+    }
+
+    String getRomanThousandth(Character thousandth) {
+        return null;
+    }
+}
---
+++ RomanNumeralConverterApi/src/test/java/com/adeniyi/rnconverter/service/RomanNumeralConverterServiceTest.java
@@ -0,0 +1,80 @@
+package com.adeniyi.rnconverter.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+
+public class RomanNumeralConverterServiceTest {
+
+    @Test
+    void testGetRomanNumeralForInteger() {
+        fail("PlaceHolder test");
+    }
+    @Test
+    void testGetRomanUnitForZero() {
+        fail("PlaceHolder test");
+    }
+    @Test
+    void testGetRomanUnitForFive() {
+        fail("PlaceHolder test");
+    }
+    @Test
+    void testGetRomanUnitForBelowFive() {
+        fail("PlaceHolder test");
+    }
+    @Test
+    void testGetRomanUnitForAboveFive() {
+        fail("PlaceHolder test");
+    }
+    @Test
+    void testGetRomanTenth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanTenthFifty() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanTenthBelowFifty() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanTenthAboveFifty() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanHundredth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanFiveHundredth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanHundredthBelowFiveHundred() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanHundredthAboveFiveHundred() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanThousandth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanThreeThousandth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanAboveThreeThousandth() {
+        fail("Test not written");
+    }
+    @Test
+    void testGetRomanBelowThreeThousandth() {
+        fail("Test not written");
+    }
+
+
+}
