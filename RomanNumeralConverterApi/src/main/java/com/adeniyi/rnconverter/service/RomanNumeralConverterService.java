259edd70eba25a3d9b4a44571713cb9fce73ba59
niyioladeji <niyioladeji@hotmail.com>
2022-07-09 04:07:59 +0100

    rnm0002 # Added additional tests and implementation for case hundreds (x100) 0 - 9 to roman numeral converter and misc

--- RomanNumeralConverterApi/src/main/java/com/adeniyi/rnconverter/service/RomanNumeralConverterService.java
+++ RomanNumeralConverterApi/src/main/java/com/adeniyi/rnconverter/service/RomanNumeralConverterService.java
@@ -1,13 +1,15 @@
 package com.adeniyi.rnconverter.service;
 
 public class RomanNumeralConverterService {
+    private static final String NULL_NUMBER_EXCEPTION = "Number can't be null";
+    private static final String INVALID_NUMBER_EXCEPTION = "Number length must be 0 - 9";
 
     public String getRomanNumeralForInteger(int unit) {
         return null;
     }
     String getRomanUnit(String unitCharacter) {
         if (unitCharacter==null) {
-            throw new IllegalArgumentException ("Number can't be null");
+            throw new IllegalArgumentException (NULL_NUMBER_EXCEPTION);
         }
         return  switch (unitCharacter) {
             case "0"-> "";
@@ -20,12 +22,12 @@ public class RomanNumeralConverterService {
             case "7"->"VII";
             case "8"-> "VIII";
             case "9"-> "IX";
-            default->throw new IllegalArgumentException ("Number length must be 0 - 9");
+            default->throw new IllegalArgumentException (INVALID_NUMBER_EXCEPTION);
         };
     }
     String getRomanTenth(String tens) {
         if (tens==null) {
-            throw new IllegalArgumentException ("Number can't be null");
+            throw new IllegalArgumentException (NULL_NUMBER_EXCEPTION);
         }
         return  switch (tens) {
             case "0"-> "";
@@ -38,11 +40,26 @@ public class RomanNumeralConverterService {
             case "7"-> "LXX";
             case "8"-> "LXXX";
             case "9"-> "XC";
-            default->throw new IllegalArgumentException ("Number length must be 0 - 9");
+            default->throw new IllegalArgumentException (INVALID_NUMBER_EXCEPTION);
         };
     }
     String getRomanHundredth(String hundredth) {
-        return null;
+        if (hundredth==null) {
+            throw new IllegalArgumentException (NULL_NUMBER_EXCEPTION);
+        }
+        return  switch (hundredth) {
+            case "0"-> "";
+            case "1"-> "C";
+            case "2"-> "CC";
+            case "3"-> "CCC";
+            case "4"-> "CD";
+            case "5"-> "D";
+            case "6"-> "DC";
+            case "7"-> "DCC";
+            case "8"-> "DCCC";
+            case "9"-> "CM";
+            default->throw new IllegalArgumentException (INVALID_NUMBER_EXCEPTION);
+        };
     }
 
     String getRomanThousandth(String thousandth) {
